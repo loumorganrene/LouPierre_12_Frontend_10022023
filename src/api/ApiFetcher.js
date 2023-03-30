b01dab4d85cadb34fe5bdf3665dea2a7375176fa
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-03-30 17:09:02 +0200

    :memo: Fetcher JSdoc

--- src/api/ApiFetcher.js
+++ src/api/ApiFetcher.js
@@ -1,42 +1,64 @@
 const BASE_URL = `http://localhost:3001/user/12`
+// TODO:
+//  2. Gérer les erreurs de l'API
+//  3. Dynamiser l'ID utilisateur via la route
 
+/**
+* @class Create a data fetcher from an API and returns the data.
+*/
 export default class ApiFetcher {
-
+    /**
+     * Get basic user data.
+     * @async
+     * @function getMainData
+     * @param {string} url - URL of the API.
+     * @returns {Promise<object>} Basic user informations.
+     */
     async getMainData() {
 
-        /**
-         * TODO: 
-         * 
-         * 1. Mettre les proptypes
-         * 2. Gérer les erreurs de l'API
-         * 3. Dynamiser l'ID utilisateur via la route
-         */
         return fetch(`${BASE_URL}`)
             .then((response) => response.json())
             .then((r => r.data))
     }
-
+    /**
+     * Get user activity data.
+     * @async
+     * @function getActivityData
+     * @param {string} url - URL of the API.
+     * @returns {Promise<object>} List of informations about user profil.
+     */
     async getActivityData() {
 
         return fetch(`${BASE_URL}/activity`)
             .then((response) => response.json())
             .then((r => r.data))
     }
-
+    /**
+     * Get user sessions duration data.
+     * @async
+     * @function getAverageSessionData
+     * @param {string} url - URL of the API.
+     * @returns {Promise<object>} List of informations about user average sessions duration.
+     */
     async getAverageSessionData() {
 
         return fetch(`${BASE_URL}/average-sessions`)
             .then((response) => response.json())
             .then((r => r.data))
     }
-
+    /**
+     * Get user performance data.
+     * @async
+     * @function getPerformanceData
+     * @param {string} url - URL of the API.
+     * @returns {Promise<object>} List of informations about user performances.
+     */
     async getPerformanceData() {
 
         return fetch(`${BASE_URL}/performance`)
             .then((response) => response.json())
             .then((r => r.data))
     }
-
 }
 
 
--- src/api/MockFetcher.js
+++ src/api/MockFetcher.js
@@ -1,27 +1,52 @@
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from "../_mocks_/mockData"
-
+/**
+* @class Create a data fetcher from mocking data and returns the data.
+*/
 export default class MockFetcher {
-
+    /**
+     * Get basic user data.
+     * @async
+     * @function getMainData
+     * @param {string} element - ID of the user.
+     * @returns {Promise<object>} Basic user informations.
+     */
     async getMainData() {
 
         return USER_MAIN_DATA.find((element) => element.id === 12)
     }
-
+    /**
+     * Get user activity data.
+     * @async
+     * @function getActivityData
+     * @param {string} element - ID of the user.
+     * @returns {Promise<object>} List of informations about user profil.
+     */
     async getActivityData() {
 
         return USER_ACTIVITY.find((element) => element.userId === 12)
     }
-
+    /**
+     * Get user sessions duration data.
+     * @async
+     * @function getAverageSessionData
+     * @param {string} element - ID of the user.
+     * @returns {Promise<object>} List of informations about user average sessions duration.
+     */
     async getAverageSessionData() {
 
         return USER_AVERAGE_SESSIONS.find((element) => element.userId === 12)
     }
-
+    /**
+     * Get user performance data.
+     * @async
+     * @function getPerformanceData
+     * @param {string} element - ID of the user.
+     * @returns {Promise<object>} List of informations about user performances.
+     */
     async getPerformanceData() {
 
         return USER_PERFORMANCE.find((element) => element.userId === 12)
     }
-
 }
 
 
